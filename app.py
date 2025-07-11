88bf1bbb9461ca704773e57bb402908f9bc8a488
lhp2901 <lehoangphuc29011986@gmail.com>
2025-07-11 09:38:11 +0700

    🚀 Initial commit for lhp-ai-server

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def home():
 
 # ──────────────── CHẠY SERVER ────────────────
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ tự set biến PORT
     app.run(host="0.0.0.0", port=port)
